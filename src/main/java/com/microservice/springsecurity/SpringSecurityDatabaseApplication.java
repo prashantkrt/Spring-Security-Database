ce4c8addca85f7c5e3c681865af8bf12dd0e3525
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-03-23 13:01:28 +0530

    Initial Commit

---
+++ src/main/java/com/microservice/springsecurity/SpringSecurityDatabaseApplication.java
@@ -0,0 +1,11 @@
+package com.microservice.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDatabaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDatabaseApplication.class, args);
+    }
+}
