afa6373d3205311e3ee588fd0fe957b266921ae6
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2025-05-09 22:33:27 +0530

    Spring Security 1

--- src/main/java/com/microservice/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/microservice/springsecurity/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.microservice.springsecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -12,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
